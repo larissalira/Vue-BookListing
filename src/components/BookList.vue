f6d5baaf3700aca7155107d5e0b81d6420cd0ef5
Larissa Lira <larissa.lira.silva@accenture.com>
2018-11-22 16:54:39 -0300

    Creating BookItem

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -2,7 +2,7 @@
     <div>
         <h1>{{title}}</h1>
         <ul>
-            <li v-for="book in books">{{book.title}}:{{book.author}}</li>
+            <li v-for="book in books"></li>
         </ul>
     </div>
 </template>
@@ -33,8 +33,4 @@ ul{
     padding: 0;
 }
 
-li{
-    display: block;
-    margin: 0 10px
-}
 </style>
